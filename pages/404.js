bc3be60a3be06cacd1d9995245633e96297b1a50
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2023-02-06 11:16:25 +0300

    Ready to deploy

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        fontSize: "50px",
+      }}
+    >
+      <h1>404 - Get out of here!</h1>
+    </div>
+  );
+}
